fe1eff7646c9f09a5c9ebe05e767a1942bf4aeb7
Ishant L <ishantlambhate@gmail.com>
2019-08-04 17:41:35 +0530

    new changes

--- CatClicker.js
+++ CatClicker.js
@@ -1,8 +1,22 @@
+var cats =[{
+  name: "cat A",
+  image: 'cat.jpg'
+},
+{
+  name: "cat B",
+  image: 'cat2.jpg'
+}]
+
 var elem = document.getElementById('cat-image');
-var clicked =0;
-elem.addEventListener('click',()=>{
-  console.log("Cat clicked");
-  clicked ++;
-  document.getElementById("show-result").innerHTML = clicked;
+var catImages = cats.map((t)=>{
+    return `<div>${t.name}</div><img src = ${t.image} alt = "cat" />`
 })
+elem.innerHTML =catImages;
+
+var clicked =0;
+ elem.addEventListener('click',()=>{
+   console.log("Cat clicked");
+   clicked ++;
+  document.getElementById("show-result").innerHTML = clicked;
+ })
 
